f775adc81b0547e44eb0e81e5ab91e87e0ea4033
Deniz <aldikactideniz@googlemail.com>
2023-05-29 01:33:49 +0200

    Account Klasse (Registrierung und Login funktioniert 100%!!) und Code wurde aufgeräumt

---
+++ GUI/Main.java
@@ -0,0 +1,15 @@
+package GUI;
+
+public class Main {
+    public static void main(String[] args) {
+        Account A = new Account("root","denizdeniz");
+        //A.registration("Hundi3","Hundi3","Hundi3");
+        A.login("Hundi3","Hundi3");
+
+
+
+        // Überprüfen der Anmeldung
+        //boolean loggedIn = account.login("testt", "password123");
+        //System.out.println("Logged in: " + loggedIn);
+    }
+}
